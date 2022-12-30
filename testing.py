f6894ef0d27ec65f943333fb85f1e783a7abe3c2
eseenandan <80133579+eseenandan@users.noreply.github.com>
2022-12-29 18:51:38 -0800

    made it so the user enter a long text and it will still be able to be copied

--- testing.py
+++ testing.py
@@ -1,7 +1,8 @@
 import pyautogui
 import time 
 import pywhatkit
-
+import win32api as fastMove
+import win32con
 
 
 pywhatkit.sendwhatmsg_instantly(phone_no="+15024963632", message="This is an automated message. You have 20 seconds to respond to use this feature.")
@@ -12,35 +13,64 @@ while True:
     position = pyautogui.position(1062,1708)
     px = pyautogui.pixel(position.x, position.y)
     top_y = position.y
+    scrolledUp = False
     
+    time.sleep(2)
+    fastMove.SetCursorPos((position.x, position.y))
+    position = pyautogui.position(position.x,position.y) # basically for getting the new position of cursor
+    pyautogui.mouseDown(None,None,button='left')
+    # if its not white it means user didnt respond 
     if px != (255,255,255):
         break
     
-    time.sleep(2)
-    pyautogui.moveTo(position.x, position.y)
-    while px == (255,255,255):
-        y = position.y - 20 # changes the y position
-        pyautogui.moveTo(position.x, y) # updates the cursor 
-        position = pyautogui.position(position.x, y) # updates the coordinates that has the x and y 
-        px = pyautogui.pixel(position.x , y) # updates the pixel checker to see if the pixels have changed
-        top_y = position.y
-        print(position)
     
-    y = top_y + 20
-    pyautogui.moveTo(position.x, y)
-    position = pyautogui.position(position.x, y)
-    pyautogui.moveTo(position.x, y)
-    px = pyautogui.pixel(position.x, y)
+    # while the pixel is white on the bottom left of the message
+    while px == (255,255,255):
+        if position.y <= 373:
+            position = pyautogui.position() # updates the coordinates that has the x and y 
+            px = pyautogui.pixel(position.x , y) # updates the pixel checker to see if the pixels have changed
+            top_y = position.y
+            print(top_y)
+            print(px)
+            scrolledUp = True
+        else:
+            y = position.y - 20 # changes the y position up 20 pixels 
+            fastMove.SetCursorPos((position.x, y))
+            position = pyautogui.position() # updates the coordinates that has the x and y 
+            px = pyautogui.pixel(position.x , y) # updates the pixel checker to see if the pixels have changed
+            top_y = position.y
+            print(position)
+            
+            
+    
+    # puts the cursor back into white area (47-63)
+    # makes the left button stop highlighting 
+    pyautogui.mouseUp()
+    pyautogui.hotkey('ctrl', 'a')
+    
+    # apparently when you scroll up you need to increase the movement of the y position more 
+    if scrolledUp == True:
+        y = top_y + 80
+    else:
+        y = top_y + 20
+        
+    # changes the y position to be in the white area again
+    fastMove.SetCursorPos((position.x, y))
+    position = pyautogui.position()
+    px = pyautogui.pixel(position.x, position.y)
+
+    # holds the mouse left down so that it can copy the text 
     pyautogui.mouseDown(None,None,button="left")
     
     while px == (255,255,255):
-        y = position.y + 20 # changes the y position
-        pyautogui.moveTo(position.x, y) # updates the cursor 
+        y = position.y + 20 # changes the y position down 20 pixels 
+        fastMove.SetCursorPos((position.x, y))
+        # pyautogui.moveTo(position.x, y) # updates the cursor 
         position = pyautogui.position(position.x, y) # updates the coordinates that has the x and y 
         px = pyautogui.pixel(position.x , y) # updates the pixel checker to see if the pixels have changed
         
-    y = position.y + 40
-    pyautogui.moveTo(position.x, y + 40)    
+    y = position.y + 80
+    pyautogui.moveTo(position.x, y)  
     pyautogui.mouseUp()
     pyautogui.hotkey('ctrl', 'c')
     pyautogui.hotkey('ctrl','t')
@@ -48,8 +78,10 @@ while True:
     pyautogui.hotkey('enter')
     print("enter pressed")
     time.sleep(5)
-    pyautogui.moveTo(1230,1769,0.5)
-    pyautogui.leftClick(1230,1769)
+    fastMove.SetCursorPos((1230,1769))
+    fastMove.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(0.01)
+    fastMove.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
     pyautogui.hotkey('ctrl', 'v')
     print("paste pressed")
     break
